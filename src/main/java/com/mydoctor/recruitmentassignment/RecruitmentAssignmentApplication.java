e213312e60dde0b8c06ebe615c6c904a3fed9477
juyeop <shininggiver@gmail.com>
2023-10-16 22:29:02 +0900

    feat: 개발 초기 세팅

---
+++ src/main/java/com/mydoctor/recruitmentassignment/RecruitmentAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.mydoctor.recruitmentassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mydoctor/recruitmentassignment/RecruitmentAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mydoctor.recruitmentassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
